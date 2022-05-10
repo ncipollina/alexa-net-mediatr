df6d9c4dcd37fb4c90f47c090cc742e1c7082718
Nicholas Cipollina <ncipollina@captechconsulting.com>
2022-05-10 08:26:48 -0400

    Added additional methods to IResponseBuilder.

--- src/Alexa.Net.MediatR/Response/IResponseBuilder.cs
+++ src/Alexa.Net.MediatR/Response/IResponseBuilder.cs
@@ -7,6 +7,7 @@ namespace Alexa.Net.MediatR.Response;
 
 public interface IResponseBuilder
 {
+    #region ResponseBuilder Wrapper Methods
     #region Tell Responses
 
     Task<SkillResponse> Tell(IOutputSpeech speechResponse, CancellationToken cancellationToken = default);
@@ -86,4 +87,65 @@ public interface IResponseBuilder
     #endregion
 
     Task<SkillResponse> Empty(CancellationToken cancellationToken = default);
+    #endregion
+
+    #region Builder Methods
+    
+    #region Speech Methods
+    
+    IResponseBuilder Speak(string? speechOutput);
+
+    IResponseBuilder Speak(params ISsml[] elements);
+
+    IResponseBuilder SpeakAudio(string? audioUrl);
+
+    IResponseBuilder Reprompt(string? repromptSpeechOutput);
+
+    IResponseBuilder Reprompt(params ISsml[] elements);
+
+    IResponseBuilder WithSimpleCard(string cardTitle, 
+        string cardContent);
+
+    IResponseBuilder WithStandardCard(string cardTitle, 
+        string cardContent, 
+        string? smallImageUrl = null, 
+        string? largeImageUrl = null);
+
+    IResponseBuilder WithLinkAccountCard();
+
+    IResponseBuilder WithAskForPermissionsConsentCard(List<string> permissionArray);
+    
+    #endregion
+
+    #region Directive Methods
+
+    IResponseBuilder AddAudioPlayerPlayDirective(PlayBehavior playBehavior, string url, string token,
+        int offsetInMilliseconds, string? expectedPreviousToken = null, AudioItemMetadata? audioItemMetadata = null,
+        CancellationToken cancellationToken = default);
+    
+    IResponseBuilder AddAudioPlayerStopDirective();
+
+    IResponseBuilder AddAudioPlayerClearQueueDirective(ClearBehavior clearBehavior);
+
+    IResponseBuilder AddConfirmIntentDirective(Intent? updatedIntent = null);
+
+    IResponseBuilder AddConfirmSlotDirective(string slotToConfirm, Intent? updatedIntent = null);
+
+    IResponseBuilder AddDelegateDirective(Intent? updatedIntent = null);
+
+    IResponseBuilder AddDirective(IDirective directive);
+
+    IResponseBuilder AddElicitSlotDirective(string slotToElicit, Intent? updatedIntent = null);
+
+    IResponseBuilder AddHintDirective(string text);
+
+    IResponseBuilder AddVideoAppLaunchDirective(string source, string? title = null, string? subtitle = null);
+
+    #endregion
+
+    IResponseBuilder WithShouldEndSession(bool val);
+
+    Task<SkillResponse> GetResponse(CancellationToken cancellationToken = default);
+
+    #endregion
 }
